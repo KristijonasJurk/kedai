4f82d1f404978dd3035be081d0e3795a9afbe4c5
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-11-19 14:14:30 +0200

    added links button and fixed gallery width issue

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 // function dropDownMenu() {
 // document.getElementsByClassName("linksButton")[0].addEventListener("click", function () {
-//     const menuHeight = document.querySelector("nav").style.height;
+//     const menuWidth = document.querySelector("nav").style.height;
 //     if (menuHeight === '0px') {
 //         document.querySelector("nav").style.height = `500px`;
 //     } else {
@@ -12,11 +12,11 @@
 // dropDownMenu()
 
 document.querySelector('.linksButton').addEventListener("click", function () {
-    const menuHeight = document.querySelector("nav").style.height;
-    if (menuHeight === '0px') {
-        document.querySelector("nav").style.height = `485px`;
+    const menu = document.querySelector("nav").style.left;
+
+    if (menu === '0px') {
+        document.querySelector("nav").style.left = `-500px`;
     } else {
-        document.querySelector("nav").style.height = `0px`;
+        document.querySelector("nav").style.left = `0px`;
     }
-    console.log('hi');
 });
