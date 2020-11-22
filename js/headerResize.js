8e291f7febee9943dbdb25dec02393cad8bfacb6
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-11-22 19:22:19 +0200

    added fixed header

--- js/headerResize.js
+++ js/headerResize.js
@@ -1,32 +1,20 @@
-function headerResize() {
-    if (document.body.scrollTop > 350) {
-        // console.log('hey');
-        // header uzrasai virsun
-        // var x = document.querySelector("header").style;
-        // x.paddingBottom = "10px";
-        // x.paddingTop = "30px";
+window.onscroll = function () { myFunction() };
 
-        // //header nuscrolinus fixed virsuj
-        // var z = document.getElementsByClassName("container")[0].style;
-        // z.backgroundColor = "#ffffff";
-        // z.position = "fixed";
-        // z.boxShadow = "0 1px 50px 5px #e7e6e63b";
-        // // link (LOGO) color change on scrolled
-        // var y = document.querySelector(".container>header a").style;
-        // y.color = "#000000";
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
 
-        // // menu uzrasu spalvos keitimas
-        // let lengthLinks = document.querySelectorAll(".container>header p").length;
-        // for (let i = 0; i < lengthLinks; i++) {
-        //     document.querySelectorAll(".container>header p")[i].style.color = "#000000";
-        // }
-
-        // // papildomi pakeitimai
-        // document.getElementsByClassName("particles-js")[0].style.height = "690px";
-        // document.querySelector(".container>header>.menuChoices").style.marginTop = "-10px";
-        // document.getElementsByClassName("menuChoices")[0].style.color = "#000";
-
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        document.querySelector(".rightside input").style.display = "none";
+        document.querySelector(".rightside a").style.display = "none";
+        document.querySelector(".rightside").style.width = "110px";
+        document.querySelector(".rightside").style.margin = "10px 0px 0px 0px";
+    } else {
+        header.classList.remove("sticky");
+        document.querySelector(".rightside input").style.display = "inline-block";
+        document.querySelector(".rightside a").style.display = "inline-block";
+        document.querySelector(".rightside").style.width = "23%";
+        document.querySelector(".rightside").style.margin = "5px 0px 0px 0px";
     }
 }
-
-// headerResize();
