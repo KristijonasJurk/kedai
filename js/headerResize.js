79088832b30928f6bb2364a0f17863623dd3764e
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-11-22 02:10:02 +0200

    added slide2 content and changed jpg to png

---
+++ js/headerResize.js
@@ -0,0 +1,32 @@
+function headerResize() {
+    if (document.body.scrollTop > 350) {
+        // console.log('hey');
+        // header uzrasai virsun
+        // var x = document.querySelector("header").style;
+        // x.paddingBottom = "10px";
+        // x.paddingTop = "30px";
+
+        // //header nuscrolinus fixed virsuj
+        // var z = document.getElementsByClassName("container")[0].style;
+        // z.backgroundColor = "#ffffff";
+        // z.position = "fixed";
+        // z.boxShadow = "0 1px 50px 5px #e7e6e63b";
+        // // link (LOGO) color change on scrolled
+        // var y = document.querySelector(".container>header a").style;
+        // y.color = "#000000";
+
+        // // menu uzrasu spalvos keitimas
+        // let lengthLinks = document.querySelectorAll(".container>header p").length;
+        // for (let i = 0; i < lengthLinks; i++) {
+        //     document.querySelectorAll(".container>header p")[i].style.color = "#000000";
+        // }
+
+        // // papildomi pakeitimai
+        // document.getElementsByClassName("particles-js")[0].style.height = "690px";
+        // document.querySelector(".container>header>.menuChoices").style.marginTop = "-10px";
+        // document.getElementsByClassName("menuChoices")[0].style.color = "#000";
+
+    }
+}
+
+// headerResize();
