45e5d4bad7c0524e32e418e45b1b2afd73e272b2
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-11-27 01:06:24 +0200

    first attempt in search bar expansion

--- js/headerResize.js
+++ js/headerResize.js
@@ -1,20 +1,30 @@
-// window.onscroll = function () { myFunction() };
+window.onscroll = function () { myFunction() };
 
-// var header = document.querySelector("header");
-// var sticky = header.offsetTop;
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
 
-// function myFunction() {
-//     if (window.pageYOffset > sticky) {
-//         header.classList.add("sticky");
-//     } else {
-//         if (window.innerWidth < 1200) {
-//             header.classList.remove("sticky");
-//         }
-//     }
-// }
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        if (window.innerWidth < 1200) {
+            header.classList.remove("sticky");
+        }
+    }
+}
 
 // const search = document.querySelector(".fa-search");
+// let searchIndex = 0;
 // search.addEventListener('click', function () {
-//     document.querySelector(".rightside input").style.display = "inline-block";
-//     document.querySelector(".rightside").style.width = "auto";
+//     searchIndex++;
+//     if (searchIndex % 2 == 0) {
+//         document.querySelector(".rightside input").style.display = "inline-block";
+//         document.querySelector(".rightside").style.width = "200px";
+//     } else {
+//         document.querySelector(".rightside input").style.display = "none";
+//         // document.querySelector(".rightside").style.width = "auto";
+//     }
 // })
+
+
+
