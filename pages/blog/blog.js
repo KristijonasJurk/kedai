fa70673e93652afa45c67c3a0646934abfbc6b00
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-11-30 22:57:32 +0200

    finished blog section

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -4,115 +4,168 @@ const dataBlog = [
         title: 'CHECK OUT OUR NEW FASHION DESIGN',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
         hashtag: 'NIKE',
-        date: '28. November 2020'
+        date: '28 November 2020'
     },
     {
         photo: '../../img/blog/blog2.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'EVERYTHING YOU NEED TO KNOW ABOUT THE LATEST ASICS',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'FASHION',
+        date: '20 November 2020'
     },
     {
         photo: '../../img/blog/blog3.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'MEET OUR NEW STUDIO JANITOR BRANDON ROY',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'STYLE',
+        date: '9 November 2020'
     },
     {
         photo: '../../img/blog/blog4.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'LACOSTE IS BACK ON TRACK, READ ALL ABOUT IT',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'LACOSTE',
+        date: '2 November 2020'
     },
     {
         photo: '../../img/blog/blog5.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'OUR NEW ALL-WHITE COLLECTION IS BRINGING IT HOT',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'COMMUNITY',
+        date: '29 September 2020'
     },
     {
         photo: '../../img/blog/blog6.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'THE MYSTERY BEHIND THESE CHINESE REEBOOKS WILL LEAVE YOU SPEECHLESS',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'STYLE',
+        date: '20 September 2020'
     },
     {
         photo: '../../img/blog/blog7.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'OLD-SCHOOL SNEEKERS ARE 23% MORE LIKELY TO GET YOU HIRED',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'SNEAKERS',
+        date: '3 September 2020'
     },
     {
         photo: '../../img/blog/blog8.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'OUR WAREHOUSE IS RUNNING OUT OF SPACE FOR NEW PRODUCTS (GIVEAWAY)',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'TRENDS',
+        date: '12 June 2020'
     },
     {
         photo: '../../img/blog/blog9.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
+        title: 'SUBWAY STORIES: THESE BEST FRIENDS MEET IN THE SUBWAY',
         text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
-    },
-    {
-        photo: '../../img/blog/blog10.png',
-        title: 'CHECK OUT OUR NEW FASHION DESIGN',
-        text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
-        hashtag: 'NIKE',
-        date: '28. November 2020'
+        hashtag: 'STREETS',
+        date: '21 May 2020'
     }
+    // {
+    //     photo: '../../img/blog/blog10.png',
+    //     title: 'ASICS ARE DROPING SOME SHOES THIS WEEKEND',
+    //     text: 'The article focuses on the lack of taste in todays fashion industry. We will analyze how this started, the state it is in now and how can we fix it...',
+    //     hashtag: 'FASHION',
+    //     date: '5 May 2020'
+    // }
 ];
 
 function generateBlog(data) {
     for (let i = 0; i < data.length; i++) {
-        const div = document.createElement('div');
-        const div11 = document.createElement('div');
-        const div1 = document.createElement('div');
-        const div2 = document.createElement('div');
-        const div3 = document.createElement('div');
-        const div4 = document.createElement('div');
+        //create all elements
+        const divBox = document.createElement('div');
         const img = document.createElement('img');
-        const a1 = document.createElement('a');
-        const a2 = document.createElement('a');
-        const a3 = document.createElement('a');
-        const a4 = document.createElement('a');
-        const text1 = document.createTextNode(data[i].label);
-        const text2 = document.createTextNode(data[i].brand);
-        const text3 = document.createTextNode(data[i].name);
-        const text4 = document.createTextNode(data[i].price);
-        const textPrice = document.createTextNode(',00 €');
-        div.setAttribute('class', 'productBox');
-        div11.setAttribute('class', 'productInfoContainer');
-        div1.setAttribute('class', 'productInfo1');
-        div2.setAttribute('class', 'productInfo2');
-        div3.setAttribute('class', 'productInfo3');
-        div4.setAttribute('class', 'productInfo4');
-        a1.appendChild(text1);
-        a2.appendChild(text2);
-        a3.appendChild(text3);
-        a4.appendChild(text4);
-        a4.appendChild(textPrice);
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        const strong = document.createElement('strong');
+        const divFooter = document.createElement('div');
+        const h4 = document.createElement('h4');
+        const divSocials = document.createElement('div');
+        const i1 = document.createElement('i');
+        const i2 = document.createElement('i');
+        const i3 = document.createElement('i');
+        //create text nodes
+        const h3Text = document.createTextNode(data[i].title);
+        const hashtag = document.createTextNode(data[i].hashtag);
+        const pText = document.createTextNode(data[i].text);
+        const h4Text = document.createTextNode(data[i].date);
         img.src = data[i].photo;
-        img.setAttribute('class', 'productPhoto');
-        div1.appendChild(a1);
-        div2.appendChild(a2);
-        div3.appendChild(a3);
-        div4.appendChild(a4);
-        div11.appendChild(div1);
-        div11.appendChild(div2);
-        div11.appendChild(div3);
-        div11.appendChild(div4);
-        div.appendChild(img);
-        div.appendChild(div11);
-        document.querySelector('.newProductsList').appendChild(div);
+        const hashtagSymbol = document.createTextNode('#');
+        //add classes
+        divBox.setAttribute('class', 'blogBox');
+        divFooter.setAttribute('class', 'blogBoxFooter');
+        i1.setAttribute('class', 'fa fa-clock-o');
+        i2.setAttribute('class', 'fa fa-twitter twitterSymbol');
+        i3.setAttribute('class', 'fa fa-facebook-square facebookSymbol');
+        // append to first level 
+        divBox.appendChild(img);
+        divBox.appendChild(h3);
+        divBox.appendChild(p);
+        divBox.appendChild(divFooter);
+        // append to second level
+        p.appendChild(strong);
+        h4.appendChild(i1);
+        divSocials.appendChild(i2);
+        divSocials.appendChild(i3);
+        // append text nodes
+        divFooter.appendChild(h4);
+        divFooter.appendChild(divSocials);
+        h3.appendChild(h3Text);
+        strong.appendChild(hashtagSymbol);
+        strong.appendChild(hashtag);
+        p.appendChild(strong);
+        p.appendChild(pText);
+        h4.appendChild(h4Text);
+        // append to html file
+        document.querySelector('.blogList').appendChild(divBox);
     }
 }
-generateNew(dataBlog);
+generateBlog(dataBlog);
+
+const boxes = document.querySelectorAll('.blogBox');
+const facebook = document.querySelectorAll('.facebookSymbol');
+const twitter = document.querySelectorAll('.twitterSymbol');
+const img = document.querySelectorAll('.blogBox img');
+const p = document.querySelectorAll('.blogBox p');
+
+boxes.forEach(function (box) {
+    box.addEventListener('mouseenter', function () {
+        box.style.boxShadow = '0px 0px 5px 0px rgb(220, 220, 220)';
+        for (let i = 0; i <= dataBlog.length; i++) {
+            if (box === boxes[i]) {
+                twitter[i].style.color = 'black';
+                facebook[i].style.color = 'black';
+                img[i].style.transform = 'scale(1.05) translateY(-7px)';
+                p[i].style.borderBottom = '1px solid rgb(220, 220, 220)';
+            }
+        }
+    })
+    box.addEventListener('mouseleave', function () {
+        box.style.boxShadow = '0px 0px 0px 0px rgb(220, 220, 220)';
+        for (let i = 0; i <= dataBlog.length; i++) {
+            if (box === boxes[i]) {
+                twitter[i].style.color = 'transparent';
+                facebook[i].style.color = 'transparent';
+                img[i].style.transform = 'scale(1) translateY(0px)';
+                p[i].style.borderBottom = '2px solid black';
+            }
+        }
+    })
+})
+
+
+
+// function renderText(data) {
+//     const largeText = 200;
+//     for (let i = 0; i <= data.length; i++) {
+//         if (data[i].text.length > largeText) {
+//             let shortText = '';
+//             for (let j = 0; j <= largeText; j++) {
+//                 shortText += data[i].text[j];
+//             }
+//         }
+//     }
+//     return shortText
+// }
+// renderText(dataBlog)
