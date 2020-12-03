6dd1ecac0e1bf2ab50432577cf6b4eeb0aca4574
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-12-04 01:48:12 +0200

    added a closable add to display discount code

--- js/header.js
+++ js/header.js
@@ -8,10 +8,49 @@ document.querySelector('.linksButton').addEventListener("click", function () {
     }
 });
 
-window.onload = function () {
-    setTimeout(loadAfterTime, 5000)
+setTimeout(openAd1, 5000);
+function openAd1() {
+    const ad1 = document.querySelector('.adContainer');
+    ad1.style.width = '55%';
+    ad1.style.transition = '2s';
+    setTimeout(fadeAdText, 1000);
+}
+function fadeAdText() {
+    const ad1Text = document.querySelectorAll('.adMessageContainer p');
+    for (let i = 0; i < ad1Text.length; i++) {
+        ad1Text[i].style.transition = '2s';
+        ad1Text[i].style.color = 'white';
+    }
 }
 
-function loadAfterTime() {
-
+function closeAd1() {
+    const ad1 = document.querySelector('.adContainer');
+    ad1.style.width = '0%';
+    ad1.style.transition = '2s';
+    const ad1Text = document.querySelectorAll('.adMessageContainer p');
+    for (let i = 0; i < ad1Text.length; i++) {
+        ad1Text[i].style.color = 'transparent';
+        ad1Text[i].style.transition = '0.1s';
+    }
 }
+
+
+function adTimer() {
+    // let countDownFromDate = new Date('Dec 18, 2021 12:00:00').toDateString();
+    let countDownDate = new Date('Sep 30, 2021 12:00:00').getTime();
+    let x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const strong = document.querySelector('.countDownAd');
+        strong.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById('accesoriesTimer').innerHTML = 'Discount is over, sorry :('
+        }
+    }, 1000)
+}
+adTimer()
