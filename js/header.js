07f180cf2f0f939aac2dd694ea2cc40c99c81e60
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-12-05 15:25:02 +0200

    fixed ad1 responsive bug

--- js/header.js
+++ js/header.js
@@ -11,15 +11,52 @@
 setTimeout(openAd1, 5000);
 function openAd1() {
     const ad1 = document.querySelector('.adContainer');
-    ad1.style.width = '55%';
-    ad1.style.transition = '2s';
-    setTimeout(fadeAdText, 1000);
+    if (window.innerWidth > 1200) {
+        ad1.style.width = '55%';
+        ad1.style.transition = '2s';
+        setTimeout(fadeAdText, 1000);
+    } if (window.innerWidth > 900 && window.innerWidth < 1200) {
+        ad1.style.width = '55%';
+        ad1.style.transition = '2s';
+        ad1.style.height = '60px';
+        setTimeout(fadeAdText, 1000);
+        setTimeout(ad1minWidth, 2000);
+    } if (window.innerWidth > 770 && window.innerWidth < 900) {
+        ad1.style.width = '55%';
+        ad1.style.transition = '2s';
+        ad1.style.height = '50px';
+        setTimeout(fadeAdText, 1000);
+        setTimeout(ad1minWidth, 2000);
+    } if (window.innerWidth < 770 && window.innerWidth > 600) {
+        ad1.style.width = '80%';
+        ad1.style.transition = '2s';
+        ad1.style.height = '70px';
+        setTimeout(fadeAdText, 1000);
+        setTimeout(ad1minWidth, 2000);
+    } if (window.innerWidth < 600) {
+        ad1.style.width = '80%';
+        ad1.style.transition = '2s';
+        ad1.style.height = '100px';
+        setTimeout(fadeAdText, 1000);
+        setTimeout(ad1minWidth, 2000);
+    }
 }
 function fadeAdText() {
     const ad1Text = document.querySelectorAll('.adMessageContainer p');
     for (let i = 0; i < ad1Text.length; i++) {
         ad1Text[i].style.transition = '2s';
         ad1Text[i].style.color = 'white';
+    }
+}
+function ad1minWidth() {
+    const ad1 = document.querySelector('.adContainer');
+    ad1.style.minWidth = '500px';
+    if (window.innerWidth > 770 && window.innerWidth < 900) {
+        ad1.style.minWidth = '490px';
+    } if (window.innerWidth < 770 && window.innerWidth > 600) {
+        ad1.style.minWidth = '475px';
+    } if (window.innerWidth < 600) {
+        ad1.style.minWidth = '300px';
     }
 }
 
@@ -32,8 +69,11 @@ function closeAd1() {
         ad1Text[i].style.color = 'transparent';
         ad1Text[i].style.transition = '0.1s';
     }
+    setTimeout(removeAd1, 1000);
 }
-
+function removeAd1() {
+    document.querySelector('.adContainer').style.display = 'none';
+}
 
 function adTimer() {
     // let countDownFromDate = new Date('Dec 18, 2021 12:00:00').toDateString();
